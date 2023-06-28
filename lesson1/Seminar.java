4acc41a12e775497972f387428bb56546a56acec
Elisha <ukaigve1990@mail.ru>
2023-06-28 18:47:11 +0300

    add dz 1, 2, 3

--- lesson1/Seminar.java
+++ lesson1/Seminar.java
@@ -1,5 +1,7 @@
 package lesson1;
 
 public class Seminar {
-    
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+    }
 }
--- program.java
+++ program.java
@@ -5,7 +5,7 @@ public class program {
     public static void main(String[] args) {
         int a = 54;
         int b = 42;
-        System.out.println(a*b);
+        System.out.println(a * b);
     }
-    
+
 }
